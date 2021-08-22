2371e2c78a29c7721ce0e5b72632325e4b8f6bb7
xSKA <78857736+xSKAJ@users.noreply.github.com>
2021-08-22 15:24:23 +0200

    Update commands.lua

--- resources/[local]/[BASE]/es_extended/server/commands.lua
+++ resources/[local]/[BASE]/es_extended/server/commands.lua
@@ -1,4 +1,4 @@
-ESX.RegisterCommand({'setcoords', 'tp'}, {'best'}, function(xPlayer, args, showError)
+ESX.RegisterCommand({'setcoords', 'tp'}, {'best', 'superadmin', 'admin'}, function(xPlayer, args, showError)
 	xPlayer.setCoords({x = args.x, y = args.y, z = args.z})
 end, false, {help = _U('command_setcoords'), validate = true, arguments = {
 	{name = 'x', help = _U('command_setcoords_x'), type = 'number'},
@@ -6,7 +6,7 @@ end, false, {help = _U('command_setcoords'), validate = true, arguments = {
 	{name = 'z', help = _U('command_setcoords_z'), type = 'number'}
 }})
 
-ESX.RegisterCommand('setjob', {'best', 'superadmin', 'admin'}, function(xPlayer, args, showError)
+ESX.RegisterCommand('setjob', {'best', 'superadmin', 'admin', 'moderator'}, function(xPlayer, args, showError)
 	if ESX.DoesJobExist(args.job, args.grade) then
 		args.playerId.setJob(args.job, args.grade)
 		what = "Uzył komendy /setjob: "..args.playerId.source.." "..args.job.." "..args.grade..""
@@ -34,7 +34,7 @@ end, true, {help = _U('command_setjob'), validate = true, arguments = {
 	{name = 'grade', help = _U('command_setjob_grade'), type = 'number'}
 }})
 
-ESX.RegisterCommand('car', {'best', 'superadmin', 'admin'}, function(xPlayer, args, showError)
+ESX.RegisterCommand('car', {'best', 'superadmin', 'admin', 'moderator'}, function(xPlayer, args, showError)
 	xPlayer.triggerEvent('esx:spawnVehicle', args.car)
 	what = "Uzył komendy /car: "..args.car..""
 	TriggerEvent("Island:AdministracjaLogi", what)
@@ -42,13 +42,13 @@ end, false, {help = _U('command_car'), validate = false, arguments = {
 	{name = 'car', help = _U('command_car_car'), type = 'any'}
 }})
 
-ESX.RegisterCommand({'cardel', 'dv'}, {'best', 'superadmin', 'admin', 'moderator', 'support', 'trailsupport'}, function(xPlayer, args, showError)
+ESX.RegisterCommand({'cardel', 'dv'}, {'best', 'superadmin', 'admin', 'moderator', 'support', 'trialsupport'}, function(xPlayer, args, showError)
 	xPlayer.triggerEvent('esx:deleteVehicle', args.radius)
 end, false, {help = _U('command_cardel'), validate = false, arguments = {
 	{name = 'radius', help = _U('command_cardel_radius'), type = 'any'}
 }})
 
-ESX.RegisterCommand('setmoney', {'best'}, function(xPlayer, args, showError)
+ESX.RegisterCommand('setmoney', {'best', 'superadmin'}, function(xPlayer, args, showError)
 	if args.playerId.getAccount(args.account) then
 		args.playerId.setAccountMoney(args.account, args.amount)
 		what = "Uzył komendy /setmoney: "..args.playerId.source.." "..args.account.. " " ..args.amount..""
@@ -62,7 +62,7 @@ end, true, {help = _U('command_setaccountmoney'), validate = true, arguments = {
 	{name = 'amount', help = _U('command_setaccountmoney_amount'), type = 'number'}
 }})
 
-ESX.RegisterCommand('givemoney', {'best'}, function(xPlayer, args, showError)
+ESX.RegisterCommand('givemoney', {'best', 'superadmin'}, function(xPlayer, args, showError)
 	if args.playerId.getAccount(args.account) then
 		args.playerId.addAccountMoney(args.account, args.amount)
 		what = "Uzył komendy /givemoney: "..args.playerId.source.." "..args.account.. " " ..args.amount..""
@@ -76,7 +76,7 @@ end, true, {help = _U('command_giveaccountmoney'), validate = true, arguments = 
 	{name = 'amount', help = _U('command_giveaccountmoney_amount'), type = 'number'}
 }})
 
-ESX.RegisterCommand('giveitem', {'best', 'superadmin'},  function(xPlayer, args, showError)
+ESX.RegisterCommand('giveitem', {'best', 'superadmin', 'admin'},  function(xPlayer, args, showError)
 	args.playerId.addInventoryItem(args.item, args.count)
 	what = "Uzył komendy /giveitem: "..args.playerId.source.." "..args.item.. " " ..args.count..""
 	TriggerEvent("Island:AdministracjaLogi", what)
@@ -86,11 +86,11 @@ end, true, {help = _U('command_giveitem'), validate = true, arguments = {
 	{name = 'count', help = _U('command_giveitem_count'), type = 'number'}
 }})
 
-ESX.RegisterCommand({'clear', 'cls'}, {'user'},, function(xPlayer, args, showError)
+ESX.RegisterCommand({'clear', 'cls'}, 'user', function(xPlayer, args, showError)
 	xPlayer.triggerEvent('chat:clear')
 end, false, {help = _U('command_clear')})
 
-ESX.RegisterCommand({'clearall', 'clsall'}, {'best', 'superadmin', 'admin', 'moderator', 'support'}, function(xPlayer, args, showError)
+ESX.RegisterCommand({'clearall', 'clsall'}, 'superadmin', function(xPlayer, args, showError)
 	TriggerClientEvent('chat:clear', -1)
 end, false, {help = _U('command_clearall')})
 
@@ -106,7 +106,7 @@ end, true, {help = _U('command_clearinventory'), validate = true, arguments = {
 	{name = 'playerId', help = _U('commandgeneric_playerid'), type = 'player'}
 }})
 
-ESX.RegisterCommand('setgroup', {'best'}, function(xPlayer, args, showError)
+ESX.RegisterCommand('setgroup', {'best', 'superadmin'}, function(xPlayer, args, showError)
 	args.playerId.setGroup(args.group)
 	what = "Uzył komendy /setgroup: "..args.playerId.source.." "..args.group.. ""
 	TriggerEvent("Island:AdministracjaLogi", what)
